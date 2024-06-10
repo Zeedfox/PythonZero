647e6bd1eecc2549a20139ba982e1d5db7e3592c
Antonio Bal <tonylodolfo@hotmail.com>
2024-06-10 16:11:34 -0600

    Añadiendo Hello World Python y print

---
+++ 00_hello.py
@@ -0,0 +1,25 @@
+# Asi se realiza un comentario con solo la almoadilla
+print("Hola mundo! Python!")
+print('Esto tambien es un texto')
+
+
+"""
+Este es un comentario
+en varias
+lineas.... 
+Recuerda usar 3 comillas al principio
+y al final
+"""
+
+
+'''
+Esto tambien
+se puede usar
+para comillas
+'''
+
+#regresa el tipo de variable
+print(type('Hola mundo')) 
+print(type(10))
+print(type(7.8))
+print(type(True))
